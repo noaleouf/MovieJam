ba27a31c8abac7036db576419ad045218e3f5773
Etudiant artsnum <artsnum@EDU.3IS.lan>
2018-04-14 04:04:01 +0200

    Added base structure for player, character and limbs

---
+++ MovieJam/Assets/Scripts/Actor/Character.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Character : MonoBehaviour {
+
+	protected uint life;
+	protected float velocity;
+	protected List<Limb> list;
+}
---
+++ MovieJam/Assets/Scripts/Actor/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : Character {
+
+	[SerializeField]
+	private NavMeshAgent aiAgent;
+
+
+
+	private Vector3 movementVec;
+	// Use this for initialization
+	void Start () {
+		movementVec = new Vector3 ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movementVec.x = Input.GetAxis("Horizontal");
+		movementVec.z = Input.GetAxis("Vertical");
+
+		print (aiAgent.transform.position + movementVec);
+
+		aiAgent.destination = aiAgent.transform.position + movementVec;
+	}
+}
